efa1d5d029fbf554045d82c0d6c0d799b954c87f
MattyHeartless <lucho.rules23@gmail.com>
2026-06-08 23:11:52 -0600

    Update environment.prod.ts
    change needed to configure coolify

--- frontend/landingpage/src/environments/environment.prod.ts
+++ frontend/landingpage/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://bienhelodias-api.ashywater-a0d6b972.westus2.azurecontainerapps.io/api'
+  apiBaseUrl: 'http://mckrw90ttro75am8ogjd8jbi.5.78.222.52.sslip.io/api'
 };
