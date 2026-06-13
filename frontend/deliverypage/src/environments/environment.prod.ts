aee59e2d5e8fee0e350d07903c9320ee6cd7269f
MattyHeartless <lucho.rules23@gmail.com>
2026-06-13 13:43:30 -0600

    Update environment.prod.ts
    apibaseurl changed to prod

--- frontend/deliverypage/src/environments/environment.prod.ts
+++ frontend/deliverypage/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7296/api',
+  apiBaseUrl: 'https://api.bienhelodias.qzz.io/api',
   push: {
     vapidPublicKey: 'BJhw8t5fPlfVnESPCQ8gSH2ZCJmPawAFd1viS-0BRNjdtcRds0modpnHKmW6mjONrwnKqqG6Q8JMxG2ws0Q_Ydg',
     registrationEnabled: true
